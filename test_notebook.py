4eed6cf158015de052698778ee2bc9ad2ed67ace
akhil.erla@gmail.com <akhil.erla@gmail.com>
2023-07-04 21:15:14 +0000

    test

---
+++ test_notebook.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+import math
+
+# COMMAND ----------
+
+
+
+def test_sqrt():
+   num = 25
+   assert math.sqrt(num) == 5
+
+
+
+# COMMAND ----------
+
+def testsquare():
+   num = 7
+   assert 7*7 == 40
+
+
+
+# COMMAND ----------
+
+def tesequality():
+   assert 10 == 11
